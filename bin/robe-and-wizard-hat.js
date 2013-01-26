5096aafbc3a50242bc3f943b4a40889169736123
ojdx <jeremy@ojdx.com>
2013-01-26 15:04:14 -0700

    update install

---
+++ bin/robe-and-wizard-hat.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+var fs = require('fs')
+    , exec = require('child_process').exec
+
+var methods = {
+    create: function (path) {
+        console.log('You put on your robe and wizard hat  ' + path)
+        fs.mkdirSync('./' + path)
+        exec('cp -R ' + __dirname + '/' + path, function (err, out) {
+            if (err) return console.log('error', err)
+            console.log('You meditate to regain your strength')
+        })
+    }
+}
+!function (args) {
+    var command = args.shift()
+    methods[command] && methods[command].apply(methods, args)
+}(process.argv.slice(2))
