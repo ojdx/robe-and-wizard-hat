4c8a8d580f4f85866f7ddd5690d6de71c99f1b8d
ojdx <jeremy@ojdx.com>
2013-01-31 16:35:58 -0700

    added mv dependency and switched to it for mv directories for windows support

--- bin/robe-and-wizard-hat.js
+++ bin/robe-and-wizard-hat.js
@@ -8,16 +8,15 @@
  * simple source copy to the created directory
  */
 var fs = require('fs'),
-    mv = require('mv')
+    wrench = require('wrench'),
+    util = require('util');
 
 var methods = {
     create: function (path) {
         console.log('You put on your robe and wizard hat')
         fs.mkdirSync('./' + path)
-        mv('../src/', './' + path, function(err){
-            if (err) return console.log('error', err)
-            console.log('You meditate to regain your mana');
-        });
+        wrench.copyDirSyncRecursive(__dirname + '/../src/', './' + path);
+        console.log('You meditate to regain your mana');
     }
 }
 !function (args) {
